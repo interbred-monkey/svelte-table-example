cbc7947b4336e81dd907a67eed9e484c6d2ef79c
Simon Mudd <smudd@amplience.com>
2021-05-05 21:23:56 +0100

    feat(svelte example): updated to have more details

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,50 @@
 <script lang="typescript">
   import ChildTable from './components/child-table/child-table.svelte';
+  import Select from './components/select/select.svelte';
   import TableComponent from './components/expandable-table/expandable-table.svelte';
   const tableData = '';
   const resolver = console.log;
 </script>
 
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
 <main>
   <h1>This is a table</h1>
-  <hr>
+  <hr />
+  <Select />
   <widget1>
     <TableComponent>
-      <div slot="childTable" >
+      <div slot="childTable">
         <ChildTable />
       </div>
     </TableComponent>
   </widget1>
-  <hr>
+  <hr />
   <widget2>
     <TableComponent data={tableData}>
-      <ChildTable resolver={resolver}/>
+      <ChildTable {resolver} />
     </TableComponent>
   </widget2>
 </main>
 
-<hr>
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<hr />
---
+++ src/components/select/select.svelte
@@ -0,0 +1,18 @@
+<script lang="typescript">
+  import Select, { Option } from '@smui/select';
+
+  let fruits = ['Apple', 'Orange', 'Banana', 'Mango'];
+  let fruitChoice = '';
+</script>
+
+<Select
+  enhanced
+  bind:value={fruitChoice}
+  label="Enhanced Input"
+  class="demo-select-width"
+  menu$class="demo-select-width">
+  <Option value="" />
+  {#each fruits as fruit}
+    <Option value={fruit} selected={fruitChoice === fruit}>{fruit}</Option>
+  {/each}
+</Select>
